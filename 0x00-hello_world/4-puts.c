e363552656fbbeb9c98138d90e55c1bbf081407b
Ahmed Rady <rady20991@gmail.com>
2023-07-13 09:28:19 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main-prog: displaty "Programming is like building a multilingual puzzle
+ * Return: 0 means success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
