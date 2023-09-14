b76b0825e47c4354c38c17e394e71b22217841e6
Ahmed Rady <rady20991@gmail.com>
2023-09-14 08:43:06 -0700

    Variadic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
+		{
 			separator = "";
+		}
 		printf("%d%s", va_arg(ap, int), separator);
 	}
 	printf("\n");
