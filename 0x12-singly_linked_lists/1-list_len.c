1c70ef516e0692b0107a9ff962e2c556c1060b2a
Ahmed Rady <rady20991@gmail.com>
2023-09-21 09:39:13 -0700

    linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_list - prints linked list
+ * @h: pointer to first node
+ * Return: size
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
