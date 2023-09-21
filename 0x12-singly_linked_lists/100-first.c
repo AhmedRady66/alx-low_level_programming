e8d85b17f711316399a47368f2cd30363e73d528
Ahmed Rady <rady20991@gmail.com>
2023-09-21 10:25:16 -0700

    linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+void beforeMain()  __attribute__((constructor));
+/**
+ * beforeMain - print before main
+ */
+void beforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
