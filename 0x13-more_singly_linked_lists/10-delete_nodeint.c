e1b9b37abc176edda21f2f7b72ced1ed3139a18f
Ahmed Rady <rady20991@gmail.com>
2023-09-25 12:32:52 -0700

    More sigly linked lists

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * @head: head of a linked list
+ * @index: key
+ * Return: 1 success
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *prev;
+	listint_t *node;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (-1);
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			prev->next = node->next;
+			free(node);
+			return (1);
+		}
+		i++;
+		prev = node;
+		node = node->next;
+	}
+	return (-1);
+}
