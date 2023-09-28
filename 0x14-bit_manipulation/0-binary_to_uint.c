a9232b2e99769adcfb7235574cb03839d025a2c2
Ahmed Rady <rady20991@gmail.com>
2023-09-28 10:15:47 -0700

    Bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,7 +7,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+	unsigned int n = 0;
 
 	if (!b)
 		return (0);
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (*b != '1' && *b != '0')
 			return (0);
-		num = num * 2 + (*b++ - '0');
+		n = n * 2 + (*b++ - '0');
 	}
-	return (num);
+	return (n);
 }
